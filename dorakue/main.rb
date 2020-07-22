a9d448f74fde3b37a65158f4d868adc1e602c2b9
Maki Miyagawa <61111655+mkmy1123@users.noreply.github.com>
2020-07-22 14:26:50 +0900

    dorakue-lesson

---
+++ dorakue/main.rb
@@ -0,0 +1,20 @@
+class Brave
+
+  def name=(name)
+    @name = name
+  end
+
+  def hp=(hp)
+    @hp = hp
+  end
+
+  def offense=(offense)
+    @offense = offense
+  end
+
+  def defense = defense
+    @defense = defense
+  end
+
+end
+
