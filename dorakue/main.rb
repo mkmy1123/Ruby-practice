89a892ccf7c6a2effec4282b23d4b068c8a30a8a
Maki Miyagawa <61111655+mkmy1123@users.noreply.github.com>
2020-07-23 15:45:12 +0900

    define brave class

--- dorakue/main.rb
+++ dorakue/main.rb
@@ -1,20 +1,41 @@
+
+
 class Brave
 
-  def name=(name)
-    @name = name
+
+  def initialize(**params)
+    @name = params[:name]
+    @hp = params[:hp]
+    @offense = params[:offense]
+    @defrnse = params[:defense]
   end
 
-  def hp=(hp)
-    @hp = hp
+  def name
+    @name
   end
 
-  def offense=(offense)
-    @offense = offense
+  def hp
+    @hp
   end
 
-  def defense = defense
-    @defense = defense
+  def offense
+    @offense
+  end
+
+  def defense
+    @defense
   end
 
 end
+
+brave = Brave.new(name: "テリー", hp: 500, offense: 150, defense: 100)
+
+
+puts <<~TEXT
+NAME: #{brave.name}
+HP: #{brave.hp}
+OFFENSE: #{brave.offense}
+DEFENSE: #{brave.defense}
+TEXT
+
 
