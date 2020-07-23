49da99eb81abb01b3559f1626c3cbd3608411497
Maki Miyagawa <61111655+mkmy1123@users.noreply.github.com>
2020-07-23 15:57:21 +0900

    define monster class

--- dorakue/main.rb
+++ dorakue/main.rb
@@ -2,6 +2,8 @@
 
 class Brave
 
+  attr_reader :name, :offense, :defense
+  attr_accessor :hp
 
   def initialize(**params)
     @name = params[:name]
@@ -10,26 +12,24 @@ class Brave
     @defrnse = params[:defense]
   end
 
-  def name
-    @name
-  end
+end
 
-  def hp
-    @hp
-  end
+class Monster
 
-  def offense
-    @offense
-  end
+  attr_reader :name, :offense, :defense
+  attr_accessor :hp
 
-  def defense
-    @defense
+  def initialize(**params)
+    @name = params[:name]
+    @hp = params[:hp]
+    @offense = params[:offense]
+    @defense = params[:defense]
   end
 
 end
 
 brave = Brave.new(name: "テリー", hp: 500, offense: 150, defense: 100)
-
+monster = Monster.new(name: "スライム", hp: 250, offense: 200, defense: 100)
 
 puts <<~TEXT
 NAME: #{brave.name}
@@ -38,4 +38,7 @@ OFFENSE: #{brave.offense}
 DEFENSE: #{brave.defense}
 TEXT
 
+brave.hp -= 30
+
+puts "#{brave.name}はダメージを受けた！ 残りHPは#{brave.hp}だ"
 
