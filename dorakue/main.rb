72a7f20e5d841ebce739a389ab90838c0563aa7c
Maki Miyagawa <61111655+mkmy1123@users.noreply.github.com>
2020-07-24 15:15:45 +0900

    implement attack method

--- dorakue/main.rb
+++ dorakue/main.rb
@@ -12,6 +12,13 @@ class Brave
     @defrnse = params[:defense]
   end
 
+  def attack(monster)
+    @offense
+    damage = @offense - monster.defense
+    monster.hp -= damage
+    puts "#{monster.name}は#{damage}のダメージを受けた"
+    puts "#{monster.name}の残りHPは#{monster.hp}だ"
+  end
 end
 
 class Monster
@@ -31,14 +38,5 @@ end
 brave = Brave.new(name: "テリー", hp: 500, offense: 150, defense: 100)
 monster = Monster.new(name: "スライム", hp: 250, offense: 200, defense: 100)
 
-puts <<~TEXT
-NAME: #{brave.name}
-HP: #{brave.hp}
-OFFENSE: #{brave.offense}
-DEFENSE: #{brave.defense}
-TEXT
-
-brave.hp -= 30
-
-puts "#{brave.name}はダメージを受けた！ 残りHPは#{brave.hp}だ"
+brave.attack(monster)
 
