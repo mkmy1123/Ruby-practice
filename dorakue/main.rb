62f3545274d69d2376a886d3afc52da4d88a16c3
Maki Miyagawa <61111655+mkmy1123@users.noreply.github.com>
2020-07-25 12:51:41 +0900

    brave refactoring

--- dorakue/main.rb
+++ dorakue/main.rb
@@ -1,5 +1,4 @@
 
-
 class Brave
 
   attr_reader :name, :offense, :defense
@@ -13,48 +12,109 @@ class Brave
     @name = params[:name]
     @hp = params[:hp]
     @offense = params[:offense]
-    @defrnse = params[:defense]
+    @defense = params[:defense]
   end
 
   def attack(monster)
     puts "#{@name}の攻撃"
 
-    attack_num = rand(4)
+    attack_type = decision_attack_type
 
-    if attack_num == 0
-      puts "必殺攻撃"
-      damage = calculate_special_attack - monster.defense
-    else
-      puts "通常攻撃"
-      damage = @offense - monster.defense
-    end
+    damage = calculate_damage(target: monster, attack_type: attack_type)
 
-    monster.hp -= damage
-    puts "#{monster.name}は#{damage}のダメージを受けた"
+    cause_damage(target: monster, damage: damage)
+
     puts "#{monster.name}の残りHPは#{monster.hp}だ"
   end
 
-  def calculate_special_attack
-    @offense * SPECIAL_ATTACK_CONSTANT
-  end
+  private
+
+    def decision_attack_type
+      attack_num = rand(4)
+
+      if attack_num == 0
+        puts "必殺攻撃"
+        "special_attack"
+      else
+        puts "通常攻撃"
+        "normal_attack"
+      end
+    end
+
+    def calculate_damage(**params)
+      target = params[:target]
+      attack_type = params[:attack_type]
+
+      if attack_type == "special_attack"
+        calculate_special_attack - target.defense
+      else
+        @offense - target.defense
+      end
+    end
+
+    def cause_damage(**params)
+      damage = params[:damage]
+      target = params[:target]
+
+      target.hp -= damage
+      puts "#{target.name}は#{damage}のダメージを受けた"
+    end
+
+    def calculate_special_attack
+      @offense * SPECIAL_ATTACK_CONSTANT
+    end
+
 end
 
 class Monster
 
-  attr_reader :name, :offense, :defense
-  attr_accessor :hp
+  attr_reader :defense, :offense
+  attr_accessor :hp, :name
+
+  POWER_UP_RATE = 1.5
+  CALC_HALF_UP = 0.5
 
   def initialize(**params)
     @name = params[:name]
     @hp = params[:hp]
     @offense = params[:offense]
     @defense = params[:defense]
+    @is_angry = false
+    @trigger_of_transform = params[:hp] * CALC_HALF_UP
   end
 
+  def attack(brave)
+    if @hp <= @trigger_of_transform && @is_angry == false
+      @is_angry = true
+      transform
+    end
+    puts "#{@name}の攻撃"
+
+    damage = @offense - brave.defense
+    brave.hp -= damage
+
+    puts "#{brave.name}は#{damage}のダメージを受けた"
+    puts "#{brave.name}の残りHPは#{brave.hp}だ"
+  end
+
+  private
+
+    def transform
+      transform_name = "ドラゴン"
+
+      puts <<~EOS
+      #{@name}は怒っている
+      #{@name}は#{transform_name}に変身した
+      EOS
+
+      @offense *= POWER_UP_RATE
+      @name = transform_name
+    end
 end
 
 brave = Brave.new(name: "テリー", hp: 500, offense: 150, defense: 100)
 monster = Monster.new(name: "スライム", hp: 250, offense: 200, defense: 100)
 
 brave.attack(monster)
+monster.attack(brave)
 
