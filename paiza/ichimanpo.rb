daa21fc8cf8a2e07bdc1b756eef9ddba06ff36ca
Maki Miyagawa <61111655+mkmy1123@users.noreply.github.com>
2020-08-12 06:21:12 +0900

    paiza

---
+++ paiza/ichimanpo.rb
@@ -0,0 +1,20 @@
+/
+入力として、歩いた距離(km)と歩幅(cm)が与えられるので、
+
+1万歩を歩いているかどうかを判定して結果を出力してください。
+/
+
+def km_to_cm(km)
+    km * 1000 * 100
+end
+
+km, cm = gets.split(' ').map(&:to_i)
+
+answer = km_to_cm(km) / cm
+
+if answer >= 10000
+    puts "yes"
+else
+    puts "no"
+end
+
