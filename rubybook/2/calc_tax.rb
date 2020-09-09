5e5f6730cacb9e387774963d0b2bf960fd4a2ead
Maki Miyagawa <61111655+mkmy1123@users.noreply.github.com>
2020-09-09 15:40:12 +0900

    rubybook 2

---
+++ rubybook/2/calc_tax.rb
@@ -0,0 +1,5 @@
+# 問8 pメソッドで途中の値段を表示させる
+total = 300 * 2
+p total
+tax = total * 0.08
+puts tax
---
+++ rubybook/2/coffee_sum.rb
@@ -0,0 +1,6 @@
+# 問6 300円のコーヒーに100円のエスプレッソを2つ加えた金額を指定の形式で表示
+coffee = 400
+espresso = 100
+count = 2
+puts "コーヒ-: #{coffee}円"
+puts "合計: #{coffee + espresso * count}円"
