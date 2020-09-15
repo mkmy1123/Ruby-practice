f9298d9b58b2134072a354850a8fc2842e701a23
Maki Miyagawa <61111655+mkmy1123@users.noreply.github.com>
2020-09-15 12:14:10 +0900

    implement deep_freeze

---
+++ cherry_book_myself/lib/deep_freezable.rb
@@ -0,0 +1,12 @@
+module DeepFreezable
+  def deep_freeze(array_or_hash)
+    case array_or_hash
+    when Array
+      array_or_hash.each(&:freeze)
+      array_or_hash.freeze
+    when Hash
+      array_or_hash.each { |key, value| key.freeze && value.freeze }
+      array_or_hash.freeze
+    end
+  end
+end
