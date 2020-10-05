e5582d4e09c19739b9c7b1eb65090f43de7272e5
Maki Miyagawa <61111655+mkmy1123@users.noreply.github.com>
2020-10-05 12:06:23 +0900

    memo_app local

--- try_sinatra/app.rb
+++ try_sinatra/app.rb
@@ -1,10 +1,35 @@
 require 'sinatra'
 require 'sinatra/reloader'
+require_relative 'lib/memo'
 
-get '/' do
-  'hello world!'
+# root page
+get "/" do
+  @memos = all_memo("memo")
+  erb :root
 end
 
-get '/erb_template_page' do
-  erb :erb_template_page
+get "/new" do
+  erb :new
+end
+
+post "/memo" do
+  create(params[:title], params[:body])
+  redirect '/'
+end
+
+def create(title, body)
+  File.open("memo/#{title}", "w", 0755) { |f| f.print body }
+end
+
+def all_memo(folder_name)
+  memos = []
+  Dir.open(folder_name).each do |file|
+    unless file[0] == "."
+      memos << {
+        title: file,
+        body: file
+      }
+    end
+  end
+  p memos
 end
---
+++ try_sinatra/lib/memo.rb
@@ -0,0 +1,8 @@
+class Memo
+  attr_accessor :title, :body
+
+  def initialize(title, body)
+    @title = title
+    @body = body
+  end
+end
